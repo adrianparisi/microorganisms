ddd3aa87c69c8c187314e88a1f32bf0aee8df0d5
Adrian Parisi <adrian.parisi@gmail.com>
2017-01-22 17:49:37 -0300

    Add initial movment to enemies.

--- Microorganisms.Core/Enemy.cs
+++ Microorganisms.Core/Enemy.cs
@@ -1,7 +1,35 @@
-﻿namespace Microorganisms.Core
+﻿using System;
+using System.Drawing;
+
+namespace Microorganisms.Core
 {
     public class Enemy : Cell
     {
+        private static Random random = new Random();
 
+
+        #region Initialization
+
+        public Enemy()
+        {
+            this.InitializeMovemnt();
+        }
+
+        private void InitializeMovemnt()
+        {
+            const int maximum = 10;
+            Point direction = Point.Empty;
+
+            while (direction == Point.Empty)
+            {
+                int x = Enemy.random.Next(-1 * maximum, maximum);
+                int y = Enemy.random.Next(-1 * maximum, maximum);
+                direction = new Point(x, y);
+            }
+
+            this.SetDirection(direction);
+        }
+
+        #endregion Initialization
     }
 }
